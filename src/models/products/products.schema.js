e9b3fa5562db6b8ac858030f26448d4d86e2f5b6
steffFrank <--replace-all>
2023-03-31 08:19:35 +0200

    feat: Add product schema

---
+++ src/models/products/products.schema.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import { uniqueValidator } from "../../utils/mongo.utils";
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    }
+});
+
+await uniqueValidator(productSchema, "name");
+
+export default mongoose.model("Product", productSchema);
